69088e22a0c5adc418919680d3ad95c1e3896b40
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-05-25 00:07:59 +0300

    OOP putting it all together

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        if isinstance(brand, str):
+            self._brand = brand
+        else:
+            print("brand must be a string")
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+stan_smith = Shoe("Adidas", 9)
+stan_smith.cobble()
+    
